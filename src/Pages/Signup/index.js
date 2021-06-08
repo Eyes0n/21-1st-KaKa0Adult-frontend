91cc0eec9eceb63fae9dd1586ceca5bbe6c60285
sanghunlee <simoniful0gmail.com>
2021-06-08 15:39:25 +0900

    Add : signup layout complete

--- src/Pages/Signup/index.js
+++ src/Pages/Signup/index.js
@@ -43,48 +43,50 @@ export default class Singup extends Component {
                 <div className="pw">
                   <strong className="pwTitle">비밀번호</strong>
                   <div className="pwBox">
-                    <label for="pwInput" className="pwLabel">
-                      <input
-                        id="pwInput"
-                        type="password"
-                        name="password"
-                        placeholder="비밀번호(8~32자리)"
-                      />
-                    </label>
+                    <label for="pwInput" className="pwLabel"></label>
+                    <input
+                      id="pwInput"
+                      type="password"
+                      name="password"
+                      placeholder="비밀번호(8~32자리)"
+                    />
                   </div>
                 </div>
                 <div className="nickname">
                   <strong className="nicknameTitle">닉네임</strong>
                   <div className="nicknameBox">
-                    <label for="nicknameInput" className="nicknameLabel">
-                      <input
-                        id="nicknameInput"
-                        type="text"
-                        name="name"
-                        placeholder="닉네임을 입력해 주세요."
-                      />
-                    </label>
+                    <label
+                      for="nicknameInput"
+                      className="nicknameLabel"
+                    ></label>
+                    <input
+                      id="nicknameInput"
+                      type="text"
+                      name="name"
+                      placeholder="닉네임을 입력해 주세요."
+                    />
                   </div>
                 </div>
                 <div className="phone">
                   <strong className="phoneTitle">전화번호</strong>
                   <div className="phoneBox">
-                    <label for="phoneInput" className="phoneLabel">
-                      <input
-                        id="phoneInput"
-                        type="text"
-                        name="phone"
-                        placeholder="전화번호를 입력해 주세요."
-                      />
-                    </label>
+                    <label for="phoneInput" className="phoneLabel"></label>
+                    <input
+                      id="phoneInput"
+                      type="text"
+                      name="phone"
+                      placeholder="전화번호를 입력해 주세요."
+                    />
                   </div>
                 </div>
+
                 <div className="birthday">
-                  <strong className="birthdayTitle">생일/성별</strong>
+                  <strong className="birthdayTitle">생일</strong>
                   <div className="birthdayBox">
+                    <label for="birthYear" className="birthLabel"></label>
                     <div className="dateBox">
                       <div className="yearSelect">
-                        <select name="birthYear" id="birthYear">
+                        <select name="year" id="birthYear">
                           <option value="">연도</option>
                           <option value="2007">2007</option>
                           <option value="2006">2006</option>
@@ -104,99 +106,94 @@ export default class Singup extends Component {
                           <option value="1992">1992</option>
                           <option value="1991">1991</option>
                         </select>
-                        <div className="monthSelect">
-                          <select name="birthMonth" id="birthMonth">
-                            <option value="">월</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
-                            <option value="11">11</option>
-                            <option value="12">12</option>
-                          </select>
-                        </div>
-                        <div className="daySelect">
-                          <select name="birthDay" id="birthDay">
-                            <option value="">일</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
-                            <option value="11">11</option>
-                            <option value="12">12</option>
-                            <option value="13">13</option>
-                            <option value="14">14</option>
-                            <option value="15">15</option>
-                            <option value="16">16</option>
-                            <option value="17">17</option>
-                            <option value="18">18</option>
-                            <option value="19">19</option>
-                            <option value="20">20</option>
-                            <option value="21">21</option>
-                            <option value="22">22</option>
-                            <option value="23">23</option>
-                            <option value="24">24</option>
-                            <option value="25">25</option>
-                            <option value="26">26</option>
-                            <option value="27">27</option>
-                            <option value="28">28</option>
-                            <option value="29">29</option>
-                            <option value="30">30</option>
-                            <option value="31">31</option>
-                          </select>
-                        </div>
                       </div>
-                      <div className="lunarBox">
-                        <input
-                          type="checkbox"
-                          name="lunarInput"
-                          id="lunarInput"
-                          value="음력"
-                        />
-                        <label for="lunarInput">음력</label>
+                      <div className="monthSelect">
+                        <select name="month" id="birthMonth">
+                          <option value="">월</option>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                          <option value="6">6</option>
+                          <option value="7">7</option>
+                          <option value="8">8</option>
+                          <option value="9">9</option>
+                          <option value="10">10</option>
+                          <option value="11">11</option>
+                          <option value="12">12</option>
+                        </select>
                       </div>
-                    </div>
-                    <div className="gender">
-                      <div className="genderBox">
-                        <input
-                          type="radio"
-                          name="gender"
-                          id="none"
-                          value="none"
-                        />
-                        <label for="none">선택안함</label>
-                        <input
-                          type="radio"
-                          name="gender"
-                          id="female"
-                          value="female"
-                        />
-                        <label for="female">여성</label>
-                        <input
-                          type="radio"
-                          name="gender"
-                          id="male"
-                          value="male"
-                        />
-                        <label for="male">남성</label>
+                      <div className="daySelect">
+                        <select name="day" id="birthDay">
+                          <option value="">일</option>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                          <option value="6">6</option>
+                          <option value="7">7</option>
+                          <option value="8">8</option>
+                          <option value="9">9</option>
+                          <option value="10">10</option>
+                          <option value="11">11</option>
+                          <option value="12">12</option>
+                          <option value="13">13</option>
+                          <option value="14">14</option>
+                          <option value="15">15</option>
+                          <option value="16">16</option>
+                          <option value="17">17</option>
+                          <option value="18">18</option>
+                          <option value="19">19</option>
+                          <option value="20">20</option>
+                          <option value="21">21</option>
+                          <option value="22">22</option>
+                          <option value="23">23</option>
+                          <option value="24">24</option>
+                          <option value="25">25</option>
+                          <option value="26">26</option>
+                          <option value="27">27</option>
+                          <option value="28">28</option>
+                          <option value="29">29</option>
+                          <option value="30">30</option>
+                          <option value="31">31</option>
+                        </select>
                       </div>
                     </div>
                   </div>
                 </div>
+                <div className="gender">
+                  <strong className="genderTitle">성별</strong>
+                  <div className="genderBox">
+                    <div className="femaleSelect">
+                      <input
+                        type="radio"
+                        name="gender"
+                        id="female"
+                        value="female"
+                      />
+                      <label for="female" className="femaleLabel">
+                        여성
+                      </label>
+                    </div>
+                    <div className="maleSelect">
+                      <input
+                        type="radio"
+                        name="gender"
+                        id="male"
+                        value="male"
+                      />
+                      <label for="male" className="maleLabel">
+                        남성
+                      </label>
+                    </div>
+                  </div>
+                </div>
                 <div className="buttonBox">
-                  <button type="submit">가입하기</button>
+                  <button type="submit" className="button">
+                    가입하기
+                  </button>
                 </div>
               </form>
             </div>
