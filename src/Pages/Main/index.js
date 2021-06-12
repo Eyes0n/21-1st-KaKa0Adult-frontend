f6588282b1bf0b9e9128e19b02e19ef8a2ff93ff
sanghunlee <simoniful0gmail.com>
2021-06-12 22:10:47 +0900

    Add: JSX 레이아웃, CSS 정리, 클릭시 페이지 전환 및 클래스 네임 state 변경

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Footer from '../../Components/Footer';
 import Nav from '../../Components/Nav';
+import MainTab from './Components/MainTab';
 import './index.scss';
+import MyPage from './MyPage';
 
 export default class Main extends Component {
   render() {
     return (
       <div className="Main">
         <Nav />
+        <div className="contentsWrap">
+          <MainTab />
+          <MyPage />
+        </div>
         <Footer />
       </div>
     );
