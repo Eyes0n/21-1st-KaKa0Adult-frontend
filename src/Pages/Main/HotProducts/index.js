257b6b7cf786f1baaedf8a21e2f6df9e67e5448d
Eys0n <eye3570@naver.com>
2021-06-16 19:31:10 +0900

    Modify : 그리드 아이템 컴포넌트 스타일 수정

--- src/Pages/Main/HotProducts/index.js
+++ src/Pages/Main/HotProducts/index.js
@@ -1,7 +1,122 @@
 import React, { Component } from 'react';
+import { API } from '../../../config';
+import { fetchDelete, fetchGet, fetchPost } from '../../../utils/fetches';
+import GridCard from './GridCard';
+import { hotData } from './hotData';
+import './index.scss';
 
-export default class index extends Component {
+export default class HotProducts extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      products: hotData.resultList,
+    });
+  }
+
+  toggleProductLike = (updatedId) => {
+    const { products } = this.state;
+
+    const updatedProducts = products.map((product) =>
+      updatedId === product.id ? { ...product, like: !product.like } : product,
+    );
+
+    this.setState({
+      products: updatedProducts,
+    });
+
+    if (products[updatedId - 1].like) {
+      fetchDelete(`${API}/users/like/product/${updatedId}`)
+        .then((res) => {
+          if (res.status === 204) {
+            alert('Like Cancle Success');
+          } else {
+            alert('Like Cancle Fail');
+          }
+        })
+        .catch((error) => console.log(error.message));
+    } else {
+      fetchPost(`${API}/users/like/product`, { product_id: updatedId })
+        .then((res) => {
+          if (res.status === 201) {
+            alert('Like Success');
+          } else {
+            alert('Like Fail');
+          }
+        })
+        .catch((error) => console.log(error.message));
+    }
+  };
+
+  onCart = (updatedId) => {
+    const { products } = this.state;
+    const updatedProducts = products.map((product) =>
+      updatedId === product.id && product.cart === false
+        ? { ...product, cart: !product.cart }
+        : product,
+    );
+
+    this.setState({
+      products: updatedProducts,
+    });
+
+    if (products[updatedId - 1].cart) {
+      fetchPost(`${API}/users/cart/product`, { product_id: updatedId })
+        .then((res) => res.json())
+        .then((result) => {
+          if (result.status === 200) {
+            alert('Add Cart Success');
+          } else {
+            alert('Add Cart Fail');
+          }
+        })
+        .catch((error) => console.log(error.message));
+    }
+  };
+
   render() {
-    return <div></div>;
+    const { products } = this.state;
+
+    return (
+      <div className="hotGridWrap">
+        <div className="hotGrid">
+          <div className="sectionGrid1">
+            {products.slice(0, 3).map((product) => (
+              <GridCard
+                key={product.id}
+                product={product}
+                onCart={this.onCart}
+                toggleProductLike={this.toggleProductLike}
+              />
+            ))}
+          </div>
+          <div className="sectionGrid2">
+            {products.slice(3, 9).map((product) => (
+              <GridCard
+                key={product.id}
+                product={product}
+                onCart={this.onCart}
+                toggleProductLike={this.toggleProductLike}
+              />
+            ))}
+          </div>
+          <div className="sectionGrid3">
+            {products.slice(9, 12).map((product) => (
+              <GridCard
+                key={product.id}
+                product={product}
+                onCart={this.onCart}
+                toggleProductLike={this.toggleProductLike}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
