30a70e3b9a1a8c2bfcefe28a0929161a1d2a2041
Eys0n <eye3570@naver.com>
2022-03-02 18:20:50 +0900

    Feat : '/' 경로 신규 아이템 페이지로 Redirect

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
 import Character from './Pages/Character';
@@ -13,6 +18,7 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
+          <Redirect exact from="/" to="/products/newList" />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/products/newList" component={NewProducts} />
