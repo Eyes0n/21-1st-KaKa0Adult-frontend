7dd2c2ee86eac53ede61f12667ed3a352b3fef93
hyunchanpark <eye3570@gmail.com>
2022-03-16 02:38:44 +0900

    [#4] feat : 캐릭터 페이지, 필터 모달 완성

--- src/Pages/Character/index.js
+++ src/Pages/Character/index.js
@@ -1,135 +1,177 @@
-import React, { Component } from 'react';
-import ProductList from '../../Components/ProductList';
+import { useState, useEffect, useMemo } from 'react';
+import { withRouter } from 'next/router';
+import ProductList from '../../components/common/ProductList';
 import FilterModal from '../../components/pageComponents/Character/FilterModal';
 import { API } from '../../config';
 import { categoryData } from '../../Data/categoryData';
 import { characterData } from '../../Data/characterData';
 import { fetchGet } from '../../utils/fetches';
 import styles from './index.module.scss';
+import Nav from '../../components/common/Nav';
+import MainTab from '../../components/common/MainTab';
 
-class Character extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isModalOpen: false,
-      categoryFilter: [],
-      characterFilter: [],
-      modalFilters: [
-        { name: '신상품순', isCheck: true },
-        { name: '판매량순', isCheck: false },
-        { name: '낮은가격순', isCheck: false },
-        { name: '높은가격순', isCheck: false },
-      ],
-      filteringName: '신상품순',
-      category: [],
-      character: [],
-    };
-  }
+const PAGE_SIZE = 10;
 
-  componentDidMount() {
-    this.setState({
-      categoryFilter: categoryData,
-      characterFilter: characterData,
-    });
-  }
+const Character = ({ router }) => {
+  const [products, setProducts] = useState({
+    list: [],
+    totalCount: 0,
+  });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [filter, setFilter] = useState({
+    categoryFilter: categoryData,
+    characterFilter: characterData,
+  });
+  const [modalFilter, setModalFilter] = useState({
+    condition: [
+      { name: '신상품순', isCheck: true },
+      { name: '판매량순', isCheck: false },
+      { name: '낮은가격순', isCheck: false },
+      { name: '높은가격순', isCheck: false },
+    ],
+    filteringName: '신상품순',
+  });
 
-  onSelectCharacter = (name) => {
-    fetchGet(`${API}/products?character=${name}`);
+  // TODO: 무한 스크롤 기능으로 page 업데이트 해주기
+  const [page, setPage] = useState(0);
+
+  const {
+    query: { type },
+  } = router;
+
+  const getCharacterDataAPI = (name, page) => {
+    fetchGet(
+      // `${API}/products?character=${name}&page=${page}&pageSize=${PAGE_SIZE}`,
+      'http://localhost:3000/data/characterData.json',
+    )
+      .then((res) => res.json())
+      .then((result) =>
+        setProducts({
+          list: result.resultList,
+          totalCount: result.totalCount,
+        }),
+      );
   };
 
-  toggleFilterModal = (e) => {
-    const { isModalOpen } = this.state;
-    const classList = [...e.target.classList];
+  useEffect(() => {
+    getCharacterDataAPI(type, page);
+  }, [page, type]);
+
+  const onSelectCharacter = (name) => {
+    router.push(`/character?type=${name}`);
+  };
+
+  const toggleFilterModal = (e) => {
+    const classListArr = [...e.target.classList];
 
     if (!isModalOpen) {
-      this.setState({ isModalOpen: !isModalOpen });
+      setIsModalOpen(true);
     } else {
-      if (classList.includes('dim')) {
-        this.setState({ isModalOpen: !isModalOpen });
+      if (classListArr.find((classList) => classList.match(/dim/))) {
+        setIsModalOpen(false);
       }
     }
   };
 
-  toggleFilterCheck = (targetId) => {
-    const { modalFilters } = this.state;
+  const FILTER_NAME_MAPPING = useMemo(
+    () => ({
+      신상품순: 'id',
+      판매량순: 'stock',
+      낮은가격순: 'price',
+      높은가격순: 'price',
+    }),
+    [],
+  );
 
-    const prevChange = modalFilters.map((el) =>
+  const selectFilterCheck = (targetIdx, name) => {
+    const prevChange = modalFilter.condition.map((el) =>
       el.isCheck ? { ...el, isCheck: !el.isCheck } : el,
     );
 
     const nextFilters = prevChange.map((el, idx) =>
-      targetId === idx ? { ...el, isCheck: !el.isCheck } : el,
+      targetIdx === idx ? { ...el, isCheck: !el.isCheck } : el,
     );
 
-    this.setState({
-      modalFilters: nextFilters,
-      filteringName: modalFilters[targetId].name,
+    setModalFilter({
+      condition: nextFilters,
+      filteringName: modalFilter.condition[targetIdx].name,
     });
+
+    // '신상품순' -> id  높은 값이 우선
+    // '판매량순' -> stock 높은 값이 우선
+    // '낮은가격순' -> price 낮은 값이 우선
+    // '높은가격순' -> price 높은 값이 우선
+    const updatedProducts = [...products.list].sort((a, b) => {
+      if (name === ('높은가격순' || '신상품순')) {
+        // 내림차순 2 -> 1
+        return b[FILTER_NAME_MAPPING[name]] - a[FILTER_NAME_MAPPING[name]];
+      }
+      // 오름차순 1 -> 2
+      return a[FILTER_NAME_MAPPING[name]] - b[FILTER_NAME_MAPPING[name]];
+    });
+
+    setProducts((prev) => ({
+      ...prev,
+      list: updatedProducts,
+    }));
+    setIsModalOpen(false);
   };
 
-  render() {
-    const { isModalOpen, modalFilters, filteringName } = this.state;
-    const { history, location, match } = this.props;
-
-    return (
-      <>
-        <section className={styles.characterWrap}>
-          <div className={styles.bannerWrap}>
-            <span className={styles.bannerTitle}>전체</span>
-            <img
-              className={styles.DropBtn}
-              src="/images/bigdropdown.png"
-              alt="dropbox"
-            />
-            <select>
-              {characterData.map((chac) => (
-                <option
-                  key={chac.id}
-                  onClick={this.onSelectCharacter(chac.name)}
-                >
-                  {chac.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className={styles.filterWrap}>
-            <div className={styles.filter}>
-              <div
-                className={styles.filterName}
-                onClick={this.toggleFilterModal}
-              >
-                <span>{filteringName}</span>
-                <img src="/images/dropdown.png" alt="dropdown" />
-              </div>
-            </div>
-            <div className={styles.filteredInfo}>
-              <div className={styles.filterTxt}>
-                <span>총</span>
-                <span>328</span>
-                <span>개</span>
-              </div>
-              <div className={styles.filterCheckBox}>
-                <img src="/images/checkIcon.png" alt="체크" />
-                <span className={styles.filterGlobaltxt}>
-                  글로벌 배송 가능상품 보기
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className={styles.listWrap}>
-            <ProductList />
-          </div>
-        </section>
-        {isModalOpen && (
-          <FilterModal
-            filters={modalFilters}
-            toggleFilterModal={this.toggleFilterModal}
-            toggleFilterCheck={this.toggleFilterCheck}
+  return (
+    <>
+      <Nav />
+      <MainTab />
+      <section className={styles.characterWrap}>
+        <div className={styles.bannerWrap}>
+          <span className={styles.bannerTitle}>{type}</span>
+          <img
+            className={styles.DropBtn}
+            src="/images/bigdropdown.png"
+            alt="dropbox"
           />
-        )}
-      </>
-    );
-  }
-}
+          <select
+            value={type}
+            onChange={(e) => onSelectCharacter(e.target.value)}
+          >
+            {filter.characterFilter.map((chac) => (
+              <option key={chac.id}>{chac.name}</option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.filterWrap}>
+          <div className={styles.filter}>
+            <div className={styles.filterName} onClick={toggleFilterModal}>
+              <span>{modalFilter.filteringName}</span>
+              <img src="/images/dropdown.png" alt="dropdown" />
+            </div>
+          </div>
+          <div className={styles.filteredInfo}>
+            <div className={styles.filterTxt}>
+              <span>총</span>
+              <span>{products.totalCount}</span>
+              <span>개</span>
+            </div>
+            <div className={styles.filterCheckBox}>
+              <img src="/images/checkIcon.png" alt="체크" />
+              <span className={styles.filterGlobaltxt}>
+                글로벌 배송 가능상품 보기
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.listWrap}>
+          <ProductList products={products.list} />
+        </div>
+      </section>
+      {isModalOpen && (
+        <FilterModal
+          filters={modalFilter.condition}
+          toggleFilterModal={toggleFilterModal}
+          selectFilterCheck={selectFilterCheck}
+        />
+      )}
+    </>
+  );
+};
 
-export default Character;
+export default withRouter(Character);
--- src/components/pageComponents/character/FilterModal/index.js
+++ src/components/pageComponents/character/FilterModal/index.js
@@ -1,62 +1,47 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from './index.module.scss';
 
-class FilterModal extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isSlide: false,
-    };
-  }
+const FilterModal = ({ filters, toggleFilterModal, selectFilterCheck }) => {
+  const [isSlide, setIsSlide] = useState(false);
 
-  componentDidMount() {
-    const { isSlide } = this.state;
-    this.setState({ isSlide: !isSlide });
-  }
+  useEffect(() => {
+    setIsSlide(true);
+  }, []);
 
-  render() {
-    const { isSlide } = this.state;
-    const { filters, toggleFilterModal, toggleFilterCheck } = this.props;
-
-    return (
-      <div
-        className={
-          isSlide
-            ? `${styles.filterModalWrap} ${styles.dim}`
-            : styles.filterModalWrap
-        }
-        onClick={toggleFilterModal}
-      >
-        <div className={styles.filterModal}>
-          <ul className={styles.filterUl}>
-            {filters.map((filter, idx) => (
-              <li
-                className={styles.filterLi}
-                key={filter.name}
-                id={idx}
-                onClick={() => toggleFilterCheck(idx)}
-              >
-                <Link
-                  href={`/products/character?category=character&search=${filter.name}&page=1&pageSize=10`}
-                >
-                  <a className={styles.fileterModalLink}>
-                    {filter.name}
-                    {filter.isCheck && (
-                      <img
-                        src="https://jotasic.github.io/21-kaka0-pet-shop-images/images/colorcheck.png"
-                        alt="check"
-                      />
-                    )}
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+  return (
+    <div
+      className={
+        isSlide
+          ? `${styles.filterModalWrap} ${styles.dim}`
+          : styles.filterModalWrap
+      }
+      onClick={toggleFilterModal}
+    >
+      <div className={styles.filterModal}>
+        <ul className={styles.filterUl}>
+          {filters.map((filter, idx) => (
+            <li
+              className={styles.filterLi}
+              key={filter.name}
+              id={idx}
+              onClick={() => selectFilterCheck(idx, filter.name)}
+            >
+              <div className={styles.fileterModalLink}>
+                {filter.name}
+                {filter.isCheck && (
+                  <img
+                    src="https://jotasic.github.io/21-kaka0-pet-shop-images/images/colorcheck.png"
+                    alt="check"
+                  />
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default FilterModal;
