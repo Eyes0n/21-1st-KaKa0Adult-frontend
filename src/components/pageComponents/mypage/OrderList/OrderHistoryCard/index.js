7c3188ec20e6e8a160ca71c64896b48cc76bf1f3
hyunchanpark <eye3570@gmail.com>
2022-03-30 13:28:25 +0900

    [#4] feat : 주문내역 컴포넌트 추가

---
+++ src/components/pageComponents/mypage/OrderList/OrderHistoryCard/index.js
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import { API } from '../../../../../config';
+import styles from './index.module.scss';
+
+const OrderHistoryCard = ({ productsArr }) => {
+  return (
+    <>
+      {productsArr?.map((products) => {
+        // products : {orderId: string, products: [{}], recipient_info: {}}
+        const { products: orderedProducts, orderId, deliveryState } = products;
+        const totalPrice = orderedProducts
+          .reduce((acc, item) => acc + Number(item.price), 0)
+          .toLocaleString();
+
+        const { id, name, imgSrc } = orderedProducts[0];
+        const length = orderedProducts.length;
+
+        return (
+          <div className={styles.contentContainer} key={id}>
+            <Link href={`${API}/order/${orderId}`}>
+              <a>
+                <div className={styles.orderInfo}>
+                  <div className={styles.image}>
+                    <img src={imgSrc} alt={name} />
+                  </div>
+                  <div className={styles.description}>
+                    <div className={styles.titleAndPrice}>
+                      <div className={styles.titleBox}>
+                        <span className={styles.title}>{name}</span>
+                        {length - 1 !== 0 && (
+                          <span>{`외 ${length - 1}건`}</span>
+                        )}
+                      </div>
+                      <p>총 결제 금액: {totalPrice}원</p>
+                    </div>
+                    <p className={styles.delivery_state}>{deliveryState}</p>
+                  </div>
+                </div>
+              </a>
+            </Link>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default OrderHistoryCard;
--- src/components/pageComponents/mypage/OrderList/index.js
+++ src/components/pageComponents/mypage/OrderList/index.js
@@ -1,9 +1,73 @@
-const OrderList = () => (
-  <div
-    style={{ display: 'flex', justifyContent: 'center', marginTop: '200px' }}
-  >
-    주문 내역
-  </div>
-);
+import { useState, useEffect } from 'react';
+import { API } from '../../../../config';
+import { fetchGet } from '../../../../utils/fetches';
+import OrderHistoryCard from './OrderHistoryCard';
+import styles from './index.module.scss';
+
+const OrderList = () => {
+  const [orderHistoies, setOrderHistoies] = useState([
+    { orderedAt: '', productsArr: [] },
+  ]);
+
+  const getOrderHistory = async () => {
+    const res = await fetchGet(`${API}/orders`);
+
+    if (res.status === 204) return;
+
+    // [{products: Array(3), recipient_info: {…}, createdAt: '2022-03-29T06:55:16.324Z'}]
+    const data = await res.json();
+
+    // [{
+    //   orderId: new Date().toString,
+    //   products: order_item_list, // [{},{}]
+    //   recipient_info: recipient_info, // {}
+    //   createdAt: new Date(), // string
+    // }]
+    const sortedByNewest = data?.reverse();
+
+    // {
+    //   'date1': [{product},{},...],
+    //   'date2': [{},{},...],
+    // }
+    const groupsByCreatedAt = sortedByNewest.reduce((groups, orderHistory) => {
+      const date = new Date(orderHistory.createdAt)
+        .toLocaleDateString()
+        .replaceAll(' ', '');
+      if (!(date in groups)) {
+        groups[date] = [];
+      }
+      groups[date].push(orderHistory);
+      return groups;
+    }, {});
+
+    // ['date1', 'date2', ...] -> [{orderedAt, productsArr}, ...]
+    const groupArrays = Object.keys(groupsByCreatedAt).map((date) => ({
+      orderedAt: date,
+      productsArr: groupsByCreatedAt[date],
+    }));
+
+    setOrderHistoies(groupArrays);
+  };
+
+  useEffect(() => {
+    getOrderHistory();
+  }, []);
+
+  return (
+    <div className={styles.wrapper}>
+      <div>
+        {orderHistoies?.map(({ orderedAt, productsArr = [] }) => {
+          console.log('productsArr', productsArr);
+          return (
+            <div key={orderedAt} className={styles.orderHistories}>
+              <p className={styles.byDate}>{orderedAt}</p>
+              <OrderHistoryCard productsArr={productsArr} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default OrderList;
